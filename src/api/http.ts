90f6fd6a2cec72bd95868aef4f3e3d6fcaae602d
YonasGetaw <yonasgetaw5444@gmail.com>
2025-12-27 20:13:34 +0300

    frontUpdated

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { env } from "../env";
 import { useAuthStore } from "../state/auth";
 
 export const http = axios.create({
-  baseURL: `${env.API_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true
 });
 
